6caac75075157b9e706b46217933e8191f1356d8
ronbrogdon-del <ronbrogdon@gmail.com>
2026-03-29 22:20:33 -0500

    Create delta_g.py

---
+++ operators/operators/delta_g.py
@@ -0,0 +1,11 @@
+"""
+ΔG Operator (Delta-G)
+Canonical deformation operator for coherence substrate evolution.
+"""
+
+class DeltaG:
+    def __init__(self):
+        pass
+
+    def compute(self, substrate_state):
+        return 0.0
